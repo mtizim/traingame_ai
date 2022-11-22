fa053286da1b10115359855686cd83d72af019e8
Szymon164 <szymonrecko1@gmail.com>
2022-11-22 21:08:01 +0100

    Add first communication and OpenCV

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.app
-
 import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import org.opencv.android.OpenCVLoader
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.flutter.result/result"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        OpenCVLoader.initDebug();
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                methodCall, result ->
+            if (methodCall.method == "EmptyList"){
+                val list = emptyList<String>();
+                result.success(list)
+            }
+        }
+    }
 }
--- app/lib/views/camera/logic/model_communication.dart
+++ app/lib/views/camera/logic/model_communication.dart
@@ -3,6 +3,7 @@
 import 'package:app/game/entities/players.dart';
 import 'package:app/game/entities/routes.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 
 class ModelResult {
   final PlayerColors color;
@@ -14,9 +15,17 @@ class ModelResult {
 // unknown input type yet
 Future<List<ModelResult>> detectRoutesFromImage(XFile image) async {
   // TODO actually implement this
-  final processing = Future.delayed(const Duration(seconds: 1));
-  final artificialDelay = Future.delayed(const Duration(seconds: 1));
 
+  const platform = const MethodChannel("com.flutter.result/result");
+
+  Future<List<Object?>> emptyList() async{
+    List<Object?> value;
+    value = await platform.invokeMethod("EmptyList");
+    return value;
+  }
+
+  final processing = Future.delayed(const Duration(seconds: 1));
+  final artificialDelay = Future.delayed(const Duration(seconds: 1),emptyList);
   await Future.wait([processing, artificialDelay]);
 
   return [
