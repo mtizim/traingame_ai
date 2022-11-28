bc9ca9a897b7c7ceef032e6b4c2b2342090996d6
Szymon164 <szymonrecko1@gmail.com>
2022-11-28 19:48:18 +0100

    Correctly loading image to Mat

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -3,20 +3,24 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import org.opencv.android.OpenCVLoader
+import org.opencv.core.*
+import org.opencv.imgcodecs.Imgcodecs
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.flutter.result/result"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        val open = OpenCVLoader.initDebug();
+        val open = OpenCVLoader.initDebug()
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
                 methodCall, result ->
             if (methodCall.method == "detectRoutes"){
-                val bytes = methodCall.argument("bytes") as ByteArray?;
-                val result_list : MutableList<String> = arrayListOf()
+                val bytes = (methodCall.argument("bytes") as ByteArray?)!!
+                val img = Imgcodecs.imdecode(MatOfByte(*bytes), Imgcodecs.IMREAD_UNCHANGED)
+
                 //model
                 //artificial jsons
+                val result_list : MutableList<String> = arrayListOf()
                 result_list.add("{\"PlayerColour\":\"red\",\"cities\":[\"Amsterdam\",\"Essen\"]}")
                 result_list.add("{\"PlayerColour\":\"black\",\"cities\":[\"Lisbon\",\"Cadiz\"]}")
                 result_list.add("{\"PlayerColour\":\"green\",\"cities\":[\"Madrid\",\"Barcelona\"]}")
