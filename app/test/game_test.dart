7bd2a2e36ab149ec6b3aef63464ee8ffc6710424
Szymon164 <szymonrecko1@gmail.com>
2022-12-17 16:20:17 +0100

    Bug fixing and tests

--- app/test/game_test.dart
+++ app/test/game_test.dart
@@ -4,6 +4,8 @@ import 'package:app/game/entities/players.dart';
 import 'package:app/game/entities/routes.dart';
 import 'package:app/game/entities/tickets.dart';
 import 'package:app/game/logic/game.dart';
+import 'package:app/game/logic/summary.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:app/game/entities/cities.dart';
 
@@ -21,7 +23,7 @@ void main() {
     MutableGameState game = MutableGameState();
     game.setPlayerRoutes(PlayerColors.red, [Routes.Sofia_Bucharest,Routes.Budapest_Bucharest]);
     game.setPlayerRoutes(PlayerColors.blue, [Routes.Athens_Smyrna]);
-    expect(game.getPlayerColors().toSet(), [PlayerColors.red,PlayerColors.blue].toSet());
+    expect(game.getPlayerColors().toSet(), {PlayerColors.red,PlayerColors.blue});
   });
   test("Adding one player with tickets", () {
     MutableGameState game = MutableGameState();
@@ -32,7 +34,7 @@ void main() {
     MutableGameState game = MutableGameState();
     game.setPlayerTicket(PlayerColors.red, Tickets.Madrid_Zurich);
     game.setPlayerTicket(PlayerColors.blue, Tickets.Athens_Ankara);
-    expect(game.getPlayerColors().toSet(), [PlayerColors.red,PlayerColors.blue].toSet());
+    expect(game.getPlayerColors().toSet(), {PlayerColors.red,PlayerColors.blue});
   });
   test("Reset game", () {
     MutableGameState game = MutableGameState();
@@ -40,5 +42,37 @@ void main() {
     game.setPlayerTicket(PlayerColors.blue, Tickets.Athens_Ankara);
     game.resetPlayers();
     expect(game.getPlayerColors().isEmpty, true);
+  });
+  test("Longest routes", () {
+    MutableGameState game = MutableGameState();
+    game.setPlayerRoutes(PlayerColors.green, [Routes.Sarajevo_Sofia]);
+    game.setPlayerRoutes(PlayerColors.blue, [Routes.Budapest_Sarajevo]);
+    game.setPlayerRoutes(PlayerColors.black, [Routes.Vienna_Budapest]);
+    game.setPlayerStations(PlayerColors.blue, [Cities.Sarajevo]);
+    for(PlayerPoints p in game.finalize().getSummary()){
+      switch(p.color){
+        case PlayerColors.blue:{
+          expect(p.maxRouteLength, 3);
+        }
+        break;
+        case PlayerColors.green:{
+          expect(p.maxRouteLength, 2);
+        }
+        break;
+        case PlayerColors.black:{
+          expect(p.maxRouteLength, 1);
+        }
+        break;
+        case PlayerColors.red:{
+          expect(p.maxRouteLength, 0);
+        }
+        break;
+        case PlayerColors.yellow:{
+          expect(p.maxRouteLength, 0);
+        }
+        break;
+      }
+
+    }
   });
 }
